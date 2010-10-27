18429213229a5f52826e07e4eec66c2265a99bcb
kindcoder <kan.kangaroo@gmail.com>
2010-10-27 12:17:13 +0300

    Changed 'topics' to 'convos'

--- WEB-INF/src/com/tah/commapp/TalkDBUtil.java
+++ WEB-INF/src/com/tah/commapp/TalkDBUtil.java
@@ -19,6 +19,7 @@ import com.mongodb.MongoException;
 
 public class TalkDBUtil {
 
+	private static final String CONVERSATIONS_COLLECTION = "convos";
 	private static Mongo mongo;
 
 	static {
@@ -39,8 +40,8 @@ public class TalkDBUtil {
 	
 	/* ------------- Talker --------------- */
 	public static void talkerConnected(int tid, String talkerId, String talkerName) {
-		if (isEmptyTopic(tid)) {
-			saveTopicAction(tid, "started");
+		if (isEmptyTalk(tid)) {
+			saveTalkAction(tid, "started");
 		}
 		
 		saveConnectionAction(tid, talkerId, "joined");
@@ -51,15 +52,15 @@ public class TalkDBUtil {
 		saveConnectionAction(tid, talkerId, "left");
 		updateLiveTalkers(tid, talkerId, talkerName, false);
 		
-		if (isEmptyTopic(tid)) {
-			saveTopicAction(tid, "finished");
+		if (isEmptyTalk(tid)) {
+			saveTalkAction(tid, "finished");
 		}
 	}
 	
 	//"talkers" - array of currently live talkers in this conversation  - [user id, userName]
 	private static void updateLiveTalkers(int tid, String talkerId, 
 			String talkerName, boolean connected) {
-		DBCollection topicsColl = getDB().getCollection("topics");
+		DBCollection convosColl = getDB().getCollection(CONVERSATIONS_COLLECTION);
 		
 		DBRef talkerRef = new DBRef(getDB(), "talkers", new ObjectId(talkerId));
 		DBObject talkerDBObject = BasicDBObjectBuilder.start()
@@ -72,13 +73,13 @@ public class TalkDBUtil {
 		if (connected) {
 			operation = "$push";
 		}
-		topicsColl.update(tidDBObject, 
+		convosColl.update(tidDBObject, 
 				new BasicDBObject(operation, new BasicDBObject("talkers", talkerDBObject)));
 	}
 	
 	//"actions" - array of different actions in this talk - user joined, left, etc
 	private static void saveConnectionAction(int tid, String talkerId, String action) {
-		DBCollection topicsColl = getDB().getCollection("topics");
+		DBCollection convosColl = getDB().getCollection(CONVERSATIONS_COLLECTION);
 		
 		DBRef talkerRef = new DBRef(getDB(), "talkers", new ObjectId(talkerId));
 		DBObject actionDBObject = BasicDBObjectBuilder.start()
@@ -88,13 +89,13 @@ public class TalkDBUtil {
 			.get();
 		
 		DBObject tidDBObject = new BasicDBObject("tid", tid);
-		topicsColl.update(tidDBObject, 
+		convosColl.update(tidDBObject, 
 				new BasicDBObject("$push", new BasicDBObject("actions", actionDBObject)));
 	}
 	
-	/* --------------- Conversation/Topic ----------------- */
+	/* --------------- Conversation ----------------- */
 	public static void saveMessage(int tid, String talkerId, String text) {
-		DBCollection topicsColl = getDB().getCollection("topics");
+		DBCollection convosColl = getDB().getCollection(CONVERSATIONS_COLLECTION);
 		
 		DBRef talkerRef = new DBRef(getDB(), "talkers", new ObjectId(talkerId));
 		DBObject commentDBObject = BasicDBObjectBuilder.start()
@@ -104,19 +105,19 @@ public class TalkDBUtil {
 			.get();
 		
 		DBObject tidDBObject = new BasicDBObject("tid", tid);
-		topicsColl.update(tidDBObject, 
+		convosColl.update(tidDBObject, 
 				new BasicDBObject("$push", new BasicDBObject("messages", commentDBObject)));
 	}
 	
 	public static List<Message> loadMessages(int tid) {
-		DBCollection topicsColl = getDB().getCollection("topics");
+		DBCollection convosColl = getDB().getCollection(CONVERSATIONS_COLLECTION);
 		
 		DBObject query = BasicDBObjectBuilder.start()
 			.add("tid", tid)
 			.get();
 			
-		DBObject topicDBObject = topicsColl.findOne(query, new BasicDBObject("messages", ""));
-		BasicDBList messagesDBList = (BasicDBList)topicDBObject.get("messages");
+		DBObject convoDBObject = convosColl.findOne(query, new BasicDBObject("messages", ""));
+		BasicDBList messagesDBList = (BasicDBList)convoDBObject.get("messages");
 		
 		List<Message> messagesList = new ArrayList<Message>();
 		if (messagesDBList == null) {
@@ -136,15 +137,15 @@ public class TalkDBUtil {
 		return messagesList;
 	}
 	
-	private static boolean isEmptyTopic(int tid) {
-		DBCollection topicsColl = getDB().getCollection("topics");
+	private static boolean isEmptyTalk(int tid) {
+		DBCollection convosColl = getDB().getCollection(CONVERSATIONS_COLLECTION);
 		
 		DBObject query = BasicDBObjectBuilder.start()
 			.add("tid", tid)
 			.get();
 			
-		DBObject topicDBObject = topicsColl.findOne(query, new BasicDBObject("talkers", ""));
-		BasicDBList liveTalkersList = (BasicDBList)topicDBObject.get("talkers");
+		DBObject talkDBObject = convosColl.findOne(query, new BasicDBObject("talkers", ""));
+		BasicDBList liveTalkersList = (BasicDBList)talkDBObject.get("talkers");
 		if (liveTalkersList == null || liveTalkersList.isEmpty()) {
 			return true;
 		}
@@ -152,8 +153,8 @@ public class TalkDBUtil {
 		return false;
 	}
 	
-	private static void saveTopicAction(int tid, String action) {
-		DBCollection topicsColl = getDB().getCollection("topics");
+	private static void saveTalkAction(int tid, String action) {
+		DBCollection convosColl = getDB().getCollection(CONVERSATIONS_COLLECTION);
 		
 		DBObject actionDBObject = BasicDBObjectBuilder.start()
 			.add("action", action)
@@ -161,12 +162,12 @@ public class TalkDBUtil {
 			.get();
 		
 		DBObject tidDBObject = new BasicDBObject("tid", tid);
-		topicsColl.update(tidDBObject, 
+		convosColl.update(tidDBObject, 
 				new BasicDBObject("$push", new BasicDBObject("topic_actions", actionDBObject)));
 	}
 	
 	public static void main(String[] args) {
-//		TalkDBUtil.talkerConnected(1, "4c2cb43160adf3055c97d061", "kangaroo");
+		TalkDBUtil.talkerConnected(43, "4c2cb43160adf3055c97d061", "kangaroo");
 //		TalkDBUtil.talkerConnected(1, "4c400cb43a10f305774734e8", "wewewe");
 //		TalkDBUtil.talkerDisconnected(1, "4c2cb43160adf3055c97d061", "kangaroo");
 //		TalkDBUtil.talkerDisconnected(1, "4c400cb43a10f305774734e8", "wewewe");
@@ -176,7 +177,7 @@ public class TalkDBUtil {
 		
 //		System.out.println(TalkDBUtil.isEmptyTopic("4c31e7a14e08f30586b71a50"));
 		
-		System.out.println(TalkDBUtil.loadMessages(1));
+//		System.out.println(TalkDBUtil.loadMessages(1));
 		
 		System.out.println("finished");
 	}
